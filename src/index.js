4413cefcf7e8095f8b287ad565bb55fa7fa73b12
Nicolas Orchow <nico@bitex.la>
2018-01-11 10:14:14 -0300

    allow to set base url

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 
-export function build_request(){
-  const headers = {
-    'Content-Type': 'application/vnd.api+json'
+export default class Client {
+  constructor(baseURL){
+    this.baseURL = baseURL
   }
 
-  return {
-    headers
+  build_request(){
+    const headers = this.build_headers()
+
+    return {
+      baseURL: this.baseURL,
+      headers
+    }
+  }
+
+  build_headers(){
+    return {
+      'Content-Type': 'application/vnd.api+json'
+    }
   }
 }
-
-const Client = {
-  build_request
-};
-
-export default Client;
--- test/index.js
+++ test/index.js
@@ -1,10 +1,17 @@
 import { expect } from 'chai';
-import client from '../src';
+import JsonApiClient from '../src';
 
 describe('jsonapi-client', function(){
   it('should send the correct Content-Type header', () => {
+    const client = new JsonApiClient();
     const request = client.build_request();
     expect(request.headers).to.have.property('Content-Type');
     expect(request.headers['Content-Type']).to.equal('application/vnd.api+json');
+  })
+
+  it('should take the initial URL as baseUrl', () => {
+    const client = new JsonApiClient('http://anyapi.com');
+    const request = client.build_request();
+    expect(request.baseURL).to.equal('http://anyapi.com');
   })
 })
