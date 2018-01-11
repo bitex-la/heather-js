0a2d792e5247f5f69c93d6cf5b1c7e4be6090947
Nicolas Orchow <nico@bitex.la>
2018-01-11 10:20:38 -0300

    Include the different allowed methods

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ export default class Client {
     this.baseURL = baseURL
   }
 
-  build_request(){
+  build_request(method){
     const headers = this.build_headers()
 
     return {
       baseURL: this.baseURL,
+      method,
       headers
     }
   }
@@ -18,5 +19,25 @@ export default class Client {
     return {
       'Content-Type': 'application/vnd.api+json'
     }
+  }
+
+  find(){
+    return this.build_request('GET');
+  }
+
+  findAll(){
+    return this.build_request('GET');
+  }
+
+  update(){
+    return this.build_request('PATCH');
+  }
+
+  create(){
+    return this.build_request('POST');
+  }
+
+  delete(){
+    return this.build_request('DELETE');
   }
 }
--- test/index.js
+++ test/index.js
@@ -14,4 +14,34 @@ describe('jsonapi-client', function(){
     const request = client.build_request();
     expect(request.baseURL).to.equal('http://anyapi.com');
   })
+
+  it('should send a GET request on find', () => {
+    const client = new JsonApiClient('http://anyapi.com');
+    const request = client.find();
+    expect(request.method).to.equal('GET');
+  })
+
+  it('should send a GET request on findAll', () => {
+    const client = new JsonApiClient('http://anyapi.com');
+    const request = client.findAll();
+    expect(request.method).to.equal('GET');
+  })
+
+  it('should send a PATCH request on update', () => {
+    const client = new JsonApiClient('http://anyapi.com');
+    const request = client.update();
+    expect(request.method).to.equal('PATCH');
+  })
+
+  it('should send a POST request on create', () => {
+    const client = new JsonApiClient('http://anyapi.com');
+    const request = client.create();
+    expect(request.method).to.equal('POST');
+  })
+
+  it('should send a DELETE request on delete', () => {
+    const client = new JsonApiClient('http://anyapi.com');
+    const request = client.delete();
+    expect(request.method).to.equal('DELETE');
+  })
 })
