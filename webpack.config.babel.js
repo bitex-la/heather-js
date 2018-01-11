268d2374d596a3d5bf7637f88df53ebe050f5274
Nicolas Orchow <nico@bitex.la>
2018-01-11 09:55:40 -0300

    fix indentation to 2 spaces

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,16 +3,16 @@ import { join } from 'path';
 const include = join(__dirname, 'src');
 
 export default {
-    entry: './src/index',
-    output: {
-       path: join(__dirname, 'dist'),
-       libraryTarget: 'umd',
-       library: 'jsonapiClient'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', include }
-        ]
-    }
+  entry: './src/index',
+  output: {
+   path: join(__dirname, 'dist'),
+   libraryTarget: 'umd',
+   library: 'jsonapiClient'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', include }
+    ]
+  }
 }
